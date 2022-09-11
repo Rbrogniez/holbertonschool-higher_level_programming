7b575e6e1616e76a4d6105c3e285016fa77f2f46
unknown <5013@holbertonschool.com>
2022-09-11 22:02:33 +0200

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 10):
-    print("{:02d}, ".format(i), end="")
-for i in range(10, 100):
-    if i % 10 > i / 10:
-        if i != 89:
-            print("{:d}, ".format(i), end="")
+for x in range(10):
+    for y in range(i+1, 10):
+        if x == 8 and y == 9:
+            print("{}{}".format(x, y))
         else:
-            print("89")
+            print("{}{}, ".format(x, y), end='')
