1bd6440389f88b395769268bc3938e60552e3a6c
Rbrogniez <5013@holbertonschool.com>
2022-09-13 16:19:16 +0200

    fix problem for the first char is equal to None

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = (len(sentence))
     first = (sentence[0])
     if length == 0:
-        return None
+        return first[None]
     else:
         return (length, first)
