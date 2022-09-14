452068f924f697e3136d7389bcbb7f596f77d533
Rbrogniez <5013@holbertonschool.com>
2022-09-14 16:18:11 +0200

    fix task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(i+1, 10):
+for x in range(0, 9):
+    for y in range(x + 1, 10):
         if x == 8 and y == 9:
-            print("{}{}".format(x, y))
+            print(f'{x}{y}'.format())
         else:
-            print("{}{}, ".format(x, y), end='')
+            print(f'{x}{y}'.format(), end=", ")
