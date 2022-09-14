6f297fa9ebcdaf6b75061cb2e73e6c474b041a6d
Rbrogniez <5013@holbertonschool.com>
2022-09-14 15:41:49 +0200

    fix PEP8

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+
     if my_list is None:
         return None
+
     else:
         my_list.reverse()
         for index in my_list:
