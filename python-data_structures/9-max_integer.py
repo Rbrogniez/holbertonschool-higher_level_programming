ea5b7a07f407c1cf62a1adc4d67d4fac6b4b0d0a
Rbrogniez <5013@holbertonschool.com>
2022-09-14 12:53:39 +0200

    add len to if and return none

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if my_list == 0:
-        my_list[0] = None
+    if len(my_list) == 0:
+        return None
     else:
         my_list.sort()
         my_list.reverse()
