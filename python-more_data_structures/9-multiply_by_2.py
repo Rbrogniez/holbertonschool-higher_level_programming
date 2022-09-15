8785d06e2ee3b1704422db1d494d007c9159c86b
Rbrogniez <5013@holbertonschool.com>
2022-09-15 16:27:28 +0200

    task09

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-from base64 import a85decode
-from re import A
-
-#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     for key in a_dictionary:
         a_dictionary[key] *= 2
