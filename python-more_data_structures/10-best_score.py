2f221f5439d7156d06cae287accdde6224316505
Rbrogniez <5013@holbertonschool.com>
2022-09-16 14:06:29 +0200

    add key=a_dict.get

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,9 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max = 0
-    for i in a_dictionary:
-        if a_dictionary[i] > max:
-            max = a_dictionary[i]
-            nom = i
-    return nom
+    return max(a_dictionary, key=a_dictionary.get)
