3fe6873609c32b7cf14da9532afeaf8ca9f7647c
Rbrogniez <5013@holbertonschool.com>
2022-09-16 12:44:29 +0200

    task 10&11

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,9 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    max = 0
+    for i in a_dictionary:
+        if a_dictionary[i] > max:
+            max = a_dictionary[i]
+            nom = i
+    return nom
