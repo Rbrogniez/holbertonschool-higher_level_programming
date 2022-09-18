83c538ae0d646f572ba7826ac8077a8556da51bf
Rbrogniez <5013@holbertonschool.com>
2022-09-18 21:32:54 +0200

    task12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from unittest import result
+
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    letter = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for x in range(len(roman_string)):
+        if x < (len(roman_string)) - 1 \
+             and letter[roman_string[x]] < letter[roman_string[x + 1]]:
+            result -= letter[roman_string[x]]
+        else:
+            result += letter[roman_string[x]]
+    return result
