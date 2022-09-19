a77c2bcfd1588f6717eb1239dd1260dd9f547e10
Rbrogniez <5013@holbertonschool.com>
2022-09-19 14:43:59 +0200

    task03

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square Module
+Private instance attribute: size.
+Instantiation with size (no type/value verification).
+"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initialize"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        return self.__size
