f246a838ce80b6906858d21f4c34bd92168da6fa
Rbrogniez <5013@holbertonschool.com>
2022-09-19 14:28:28 +0200

    fix line 12 , change size by type(size)

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,10 +9,10 @@ class Square:
     """Square Class"""
     def __init__(self, size=0):
         """Initialize"""
-        if (type) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size >= 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        
+
