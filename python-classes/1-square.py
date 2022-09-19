edf3ce27ea75836fc161b5f94d99388007fa25d7
Rbrogniez <5013@holbertonschool.com>
2022-09-19 11:42:27 +0200

    add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,5 +8,6 @@ Instantiation with size (no type/value verification).
 class Square:
     """Square Class"""
     def __init__(self, size):
+        """Initialize"""
         self.__size = size
         pass
