24d9ecd020b04562927f2cc19fc190f573d82778
Rbrogniez <5013@holbertonschool.com>
2022-09-19 11:41:29 +0200

    add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Square Module
+Private instance attribute: size.
+Instantiation with size (no type/value verification).
+"""
+
 
 class Square:
+    """Square Class"""
     def __init__(self, size):
         self.__size = size
         pass
