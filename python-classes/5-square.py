7a3ea73f564afa75ec1d21cabdfe277bd6d91d4a
Rbrogniez <5013@holbertonschool.com>
2022-09-20 15:37:13 +0200

    task05

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Square Module
+Private instance attribute: size.
+Instantiation with size (no type/value verification).
+"""
+
+
+class Square:
+
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """Initialize"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Square of area"""
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+
+                for y in range(self.__size):
+                    print("#", end="")
+                print("")
+
