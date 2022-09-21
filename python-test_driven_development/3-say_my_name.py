73713e15a35019f456b7b164c5ee2d0c9d9738a9
Rbrogniez <5013@holbertonschool.com>
2022-09-21 16:47:14 +0200

    add doc and fix PEP8

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+This print name and last name module
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    return first name or last name or error if not a string
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     else:
         print("My name is {} {}".format(first_name, last_name))
-
