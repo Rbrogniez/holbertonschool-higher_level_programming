07d4ae1c6026fbcc24a64c224319bc7aedce901e
Rbrogniez <5013@holbertonschool.com>
2022-09-21 17:04:17 +0200

    task04

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+from typing import Type
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if type(size) is float and size <=0:
+        raise TypeError("size must be an integer")
+    else:
+        for i in range(size):
+            for x in range(size):
+                print("#", end="")
+            print()
+
