2de5b3d7f924c258276899fb6028af37691e8840
Rbrogniez <5013@holbertonschool.com>
2022-09-21 15:54:46 +0200

    add else

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,5 +13,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    return a + b
+    else:
+        return a + b
