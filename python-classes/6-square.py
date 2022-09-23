cfb050b63423e615534e7463a6686d0c2eca0a8c
Rbrogniez <5013@holbertonschool.com>
2022-09-23 14:41:01 +0200

    fix PEP8

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -9,40 +9,56 @@ class Square:
 
     """Square Class"""
 
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize"""
-        self.__size = size
-        self.__position = position
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        if type(position) is not tuple or len(position) != 2 \
+            or type(position[0]) is not int or position[0] < 0 \
+                or type(position[1]) is not int or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        """Area of the square"""
+        return self.__size**2
 
     @property
     def size(self):
-        """retrieve size"""
+        """Get the size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setter size"""
+        """Set the size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     @property
     def position(self):
-        return(self.__position)
+        """Get the position"""
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) is not int:
+        """Set the position"""
+        if type(value) is not tuple or len(value) != 2 \
+            or type(value[0]) is not int or value[0] < 0 \
+                or type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
-
-    def area(self):
-        """Square of area"""
-        return self.__size**2
-
+            
     def my_print(self):
         if self.__size == 0:
             print()
