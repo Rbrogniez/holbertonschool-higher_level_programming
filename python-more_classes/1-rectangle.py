0164da01277c91e1980ea4407fd6230710eb0a53
Rbrogniez <5013@holbertonschool.com>
2022-09-23 15:40:12 +0200

    add if for width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,6 +11,18 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize"""
+
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
 
@@ -24,7 +36,7 @@ class Rectangle:
         """setter width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -33,14 +45,14 @@ class Rectangle:
     @property
     def height(self):
         """retrieve height"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """setter height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
