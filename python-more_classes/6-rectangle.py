258b93787b21b82b1af2a561605f3d26b63c4a47
Rbrogniez <5013@holbertonschool.com>
2022-09-25 23:01:06 +0200

    task06

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,11 +4,12 @@ Rectangle Module
 
 """
 
-number_of_instances = 0
 
 class Rectangle:
 
     """"Rectangle Class"""
+
+    instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize"""
@@ -26,7 +27,7 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        Rectangle.number_of_instances -= 1
+        Rectangle.instances -= 1
 
     @property
     def width(self):
@@ -81,4 +82,4 @@ class Rectangle:
     def __del__(self):
         """Delete a Rectangle"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        Rectangle.instances -= 1
