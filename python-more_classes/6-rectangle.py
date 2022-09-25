fb56a78eeb27d8ccd988be20a66986efb3c3a6f5
Rbrogniez <5013@holbertonschool.com>
2022-09-25 23:02:31 +0200

    fix

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
 
     """"Rectangle Class"""
 
-    instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize"""
@@ -27,7 +27,7 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        Rectangle.instances -= 1
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -82,4 +82,4 @@ class Rectangle:
     def __del__(self):
         """Delete a Rectangle"""
         print("Bye rectangle...")
-        Rectangle.instances -= 1
+        Rectangle.number_of_instances -= 1
