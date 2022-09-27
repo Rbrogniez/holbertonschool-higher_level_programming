aabd8d8b619687c1884a841243618268503be301
Rbrogniez <5013@holbertonschool.com>
2022-09-27 15:29:52 +0200

    negative test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,8 +3,9 @@
 Module My list
 """
 
+
 class MyList(list):
     """My List Class"""
-    def print_sorted(self):
+    def print_sorted (self):
         """ prints the list, but sorted """
         print (sorted(self))
