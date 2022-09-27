91c103305b9f897bbaf9e2302a6554ab4da337fc
Rbrogniez <5013@holbertonschool.com>
2022-09-27 15:59:37 +0200

    add comm

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Module same classe
+"""
+
+
 def is_same_class(obj, a_class):
+    """ eturns True if the object is exactly an instance of the specified class
+    otherwise False."""
     if type(obj) is a_class:
         return (True)
     else:
