3165c84091f5dded8af8eb0ecc93165866773ec6
Rbrogniez <5013@holbertonschool.com>
2022-09-27 16:54:59 +0200

    add doc

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
-import re
+"""
+Module is kind of class
+"""
 
 
 def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of, or if the object is
+    an instance of a class that inherited from, the specified class ;
+    otherwise False."""
     if isinstance(obj,int):
         return (True)
     elif issubclass(a_class,int):
