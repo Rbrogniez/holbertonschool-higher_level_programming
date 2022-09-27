05202296a9f58eb42d1f7773b4d47e017e3f559c
Rbrogniez <5013@holbertonschool.com>
2022-09-27 17:19:01 +0200

    add com

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,9 +5,8 @@ Module inherts from
 
 
 def is_kind_of_class(obj, a_class):
-    """unction that returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class ;
-    otherwise False."""
+    """returns True if the object is an instance of a class
+    that inherited from the specified class"""
     if issubclass(obj, a_class):
         return (True)
     else:
