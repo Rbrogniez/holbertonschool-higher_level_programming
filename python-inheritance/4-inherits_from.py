4f97f324ee4a648b6531e4a4db533ddf784f62f2
Rbrogniez <5013@holbertonschool.com>
2022-09-28 10:58:39 +0200

    add type before obj

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class
     that inherited from the specified class"""
-    if issubclass(obj, a_class) and type(obj) is not a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return (True)
     else:
         return (False)
