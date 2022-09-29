64786c65d6b73dae4db7fcffbc7cb930879f2d06
Rbrogniez <5013@holbertonschool.com>
2022-09-29 14:57:35 +0200

    fix PEP8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 Module Geometry
 """
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -16,6 +15,3 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
-
-
