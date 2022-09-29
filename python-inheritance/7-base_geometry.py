4cf058f20163da736461d291da00e2e3c601780d
Rbrogniez <5013@holbertonschool.com>
2022-09-29 14:11:26 +0200

    delete and recreat files , it's work

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,13 +5,17 @@ Module Base Geometry
 
 
 class BaseGeometry:
+
     """Class Base Geometry"""
     def area(self):
+
         """Raises an exception"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        if value is not int:
-            raise TypeError("<name> must be an integer")
+        """validate value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
 
