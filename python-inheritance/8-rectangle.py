6f8ae62c295ec2849daa78fab339d20dc45f82fe
Rbrogniez <5013@holbertonschool.com>
2022-09-29 15:03:01 +0200

    fix document

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
     """ Class Rectangle"""
 
     def __init__(self, width, height):
-        "Initialize"
+        """Initialize"""
 
         self.__width = width
         self.__height = height
