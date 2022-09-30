1dad442e124367c4516174d0b29423c9027b2bf9
Rbrogniez <5013@holbertonschool.com>
2022-09-30 15:09:11 +0200

    add task10 & 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f'[Square] {self.__size}/{self.__size}'
+
+
+
+
