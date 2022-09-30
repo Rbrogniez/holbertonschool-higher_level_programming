37519532ed99ea2acf5985f60006081a23038287
Rbrogniez <5013@holbertonschool.com>
2022-09-30 10:34:35 +0200

    fix doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Module Read file"""
+"""
+Module Read file
+"""
 
 
 def read_file(filename=""):
-    """Function Read a file"""
+    """
+    Function Read a file
+    """
 with open(filename, "r+") as file:
     print(file.readlines())
     file.close()
