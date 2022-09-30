a28a27a343c310927fa7e2933832410d6b29aef4
Rbrogniez <5013@holbertonschool.com>
2022-09-30 10:28:03 +0200

    Fix doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Module Read File
-"""
+"""Module Read file"""
 
 
 def read_file(filename=""):
-    """function that reads a text file (UTF8) and prints it to stdout:"""
-
-
+    """Function Read a file"""
 with open(filename, "r+") as file:
     print(file.readlines())
     file.close()
