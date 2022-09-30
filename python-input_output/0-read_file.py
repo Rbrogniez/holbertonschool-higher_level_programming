f0fc8168c6e9305d0c0874f181f1b45060fd814a
Rbrogniez <5013@holbertonschool.com>
2022-09-30 10:23:43 +0200

    add README and Task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Module Read File
+"""
+
 
 def read_file(filename=""):
-   with open(filename, "r+") as file:
+    """function that reads a text file (UTF8) and prints it to stdout:"""
+
+
+with open(filename, "r+") as file:
     print(file.readlines())
     file.close()
