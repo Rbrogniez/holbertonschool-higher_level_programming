cf57602e90f3bc6a4d1cb5375bc8e30bbe3084f3
Rbrogniez <5013@holbertonschool.com>
2022-09-30 11:29:13 +0200

    fix PEP8

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,12 +3,12 @@
 Module Append to a file
 """
 
+
 def append_write(filename="", text=""):
     """
      function that appends a string at the end of a text file (UTF8)
      and returns the number of characters added:
     """
-    with open(filename,"a+",encoding='utf-8') as file:
+    with open(filename, "a+", encoding='utf-8') as file:
         file.write(text)
         return len(text)
-
