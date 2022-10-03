18a543788d7742a256510484925c795fc44f471d
Rbrogniez <5013@holbertonschool.com>
2022-10-03 17:05:05 +0200

    fix ValueError message

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,16 +10,6 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """inizializes a Rectangle"""
 
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be >= 0")
-
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be >= 0")
-
         self.width = width
         self.height = height
         self.x = x
@@ -38,7 +28,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
@@ -52,7 +42,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
