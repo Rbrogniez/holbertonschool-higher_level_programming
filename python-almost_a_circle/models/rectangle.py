5c36f221d4c46d66e546d78685624e816dd5d407
Rbrogniez <5013@holbertonschool.com>
2022-10-03 17:00:53 +0200

    add <= to ValueError

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        
+
 
     @property
     def width(self):
@@ -27,7 +27,7 @@ class Rectangle(Base):
         """setter width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -41,7 +41,7 @@ class Rectangle(Base):
         """setter height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
