e699d760a9494ccf167e7c8d35f117a0c46a7d47
Rbrogniez <5013@holbertonschool.com>
2022-10-03 15:02:49 +0200

    add self.id = base.nb_objects

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,24 +4,21 @@ Module Base Classe
 """
 
 
-class base():
+class Base:
 
-    """Base Class"""
-
-
-__nb_objects = 0
+    __nb_objects = 0
 
 
-def __init__(self, id=None):
 
-    """if id is not None, assign the public instance attribute id with this
-    argument value
-    otherwise, increment __nb_objects and assign the new value to the public
-    """
+    def __init__(self, id=None):
 
-    if id is not None:
-        self.__id = id
-    else:
-        self.__nb_objects += 1
-        self.id = id
+        """if id is not None, assign the public instance attribute id with this
+        argument value
+        otherwise, increment __nb_objects and assign the new value to the public
+        """
 
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
