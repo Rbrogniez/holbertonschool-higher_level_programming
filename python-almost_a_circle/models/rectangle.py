692def13088fd0c9b5a642552b7cbb5966b2f802
Rbrogniez <5013@holbertonschool.com>
2022-10-03 16:56:46 +0200

    try add in init execption

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,17 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+
     @property
     def width(self):
         return self.__width
