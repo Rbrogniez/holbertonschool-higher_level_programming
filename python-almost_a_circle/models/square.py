c69d049e0b3b2bbea6d09b5532adcc6e8527cf1d
Rbrogniez <5013@holbertonschool.com>
2022-10-05 13:51:09 +0200

    revome str

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,7 +11,3 @@ class Square(Rectangle):
         super().__init__(size,size,x,y,id)
         self.size = size
 
-
-    def __str__(self):
-        return f'[Square]({self.id}) {self.x}/{self.y} - {self.size}'
-
