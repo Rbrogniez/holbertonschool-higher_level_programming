c27affbb717b10e4163f66c37890a70b5ad4e23a
Rbrogniez <5013@holbertonschool.com>
2022-10-05 13:47:52 +0200

    add rectangle and square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,5 +8,10 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
 
-        super().__init__(size,x,y,id)
+        super().__init__(size,size,x,y,id)
+        self.size = size
+
+
+    def __str__(self):
+        return f'[Square]({self.id}) {self.x}/{self.y} - {self.size}'
 
