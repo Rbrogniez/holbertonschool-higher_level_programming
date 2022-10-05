234db7277c1fb7ed3ae922dda02021a4096f108b
Rbrogniez <5013@holbertonschool.com>
2022-10-05 14:08:08 +0200

    fix PEP8

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,15 +2,14 @@
 """Module Square"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
 
-        super().__init__(size,size,x,y,id)
+        super().__init__(size, size, x, y, id)
         self.size = size
-
 
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
-
