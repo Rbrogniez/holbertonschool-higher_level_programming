576f5e145a523bdc7ac5029da8abeb1cf4b64d7e
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2022-10-07 15:09:21 +0200

    Add files via upload

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Module add item"""
-from sys import argv
+"""
+module to import arguments json
+
+"""
+from sys import argv as av
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    items = load_from_json_file("add_item.json")
-except Exception:
-    items = []
 
-argc = len(argv)
-if argc > 1:
-    for i in range(1, argc):
-        items.append(argv[i])
-save_to_json_file(items, "add_item.json")
+try:
+    P_list = load_from_json_file('add_item.json')
+except Exception:
+    P_list = []
+for i in av[1:]:
+    P_list.append(i)
+
+save_to_json_file(P_list, 'add_item.json')
