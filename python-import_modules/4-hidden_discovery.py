55393ca5a3aedc4febf36beb1749bef58ae09935
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2022-10-25 23:43:20 +0200

    Delete python-import_modules directory

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-
-    text = dir(hidden_4)
-
-    for word in text:
-        if word[0:2] != "__":
-            print("{:s}".format(word))
