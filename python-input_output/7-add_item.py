b059e1d426e45ed37b7ac17358b42a3609da7328
unknown <5013@holbertonschool.com>
2022-10-25 22:02:08 +0200

    task 07 and 12

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-module to import arguments json
-
-"""
-from sys import argv as av
-import json
+"""Module add item"""
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 try:
-    P_list = load_from_json_file('add_item.json')
+    items = load_from_json_file("add_item.json")
 except Exception:
-    P_list = []
-for i in av[1:]:
-    P_list.append(i)
+    items = []
 
-save_to_json_file(P_list, 'add_item.json')
+argc = len(argv)
+if argc > 1:
+    for i in range(1, argc):
+        items.append(argv[i])
+save_to_json_file(items, "add_item.json")
