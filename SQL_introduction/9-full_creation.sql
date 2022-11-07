7316bf97f162896235d0010abe050e247ac50c8a
Rbrogniez <5013@holbertonschool.com>
2022-11-07 10:55:45 +0100

    full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- Task description : Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREAT TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(232),
+	score INT
+);
+
+INSERT INTO second_table(
+	id,
+	name,
+	score
+);
+
+VALUES
+(1,John,10),
+(2,Alex,3),
+(3,Bob,14),
+(4,George,8);
