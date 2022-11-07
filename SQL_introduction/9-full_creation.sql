8f7e4290f71df3edba3648c7016cd927607df765
Rbrogniez <5013@holbertonschool.com>
2022-11-07 11:18:16 +0100

    add ''

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Task description : Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-CREAT TABLE IF NOT EXISTS second_table(
+CREAT TABLE IF NOT EXISTS 'second_table'(
 	id INT,
 	name VARCHAR(232),
 	score INT
