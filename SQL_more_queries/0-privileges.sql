d3f3c93af0d0389c6e22518c8876fde4f3178180
unknown <5013@holbertonschool.com>
2022-11-08 12:19:18 +0100

    add show grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
---  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server 
-SELECT CONCAT('SHOW GRANTS FOR ''',user,'''@''',host,''';') FROM mysql.user;
+--  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+SHOW GRANTS FOR 'root'@'user_0d_1';
+SHOW GRANTS FOR 'root'@'user_0d_2';
