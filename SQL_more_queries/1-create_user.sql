3218a6ca01c076d033b925dd9e9dd2b8b33120b2
Rbrogniez <5013@holbertonschool.com>
2022-11-08 14:50:02 +0100

    give all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Task description : Write a script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
