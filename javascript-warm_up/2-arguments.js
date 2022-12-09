8c58b32226e18c404fdbf992df1f38640c6e96a1
Rbrogniez <5013@holbertonschool.com>
2022-12-09 14:22:34 +0100

    adding =

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args .length = 0) {
+if (args .length == 0) {
   console.log('No argument');
 }
-if (args.length = 1) {
+if (args.length == 1) {
   console.log('No argument');
 } else {
   console.log('Argument found');
