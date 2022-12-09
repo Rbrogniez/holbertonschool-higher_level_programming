921d8bd9e9413af2da9ff364f7314e9aa81bfd95
Rbrogniez <5013@holbertonschool.com>
2022-12-09 15:23:24 +0100

    change === by >

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 2) {
-  console.log('No argument');
+if (args.length > 3) {
+  console.log('Aguments found');
 } else if (args.length === 3) {
   console.log('Argument found');
 } else {
