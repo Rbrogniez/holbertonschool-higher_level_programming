9ed443a9f23650c910bc542d93b995ec0c42ea7f
Rbrogniez <5013@holbertonschool.com>
2022-12-09 14:20:00 +0100

    adding length avec args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args = 0) {
+if (args .length = 0) {
   console.log('No argument');
 }
-if (args = 1) {
+if (args.length = 1) {
   console.log('No argument');
 } else {
   console.log('Argument found');
