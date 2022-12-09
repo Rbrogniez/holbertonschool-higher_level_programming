09bffedb3a13c7d736478e9c17ea0d5d177d2e1f
Rbrogniez <5013@holbertonschool.com>
2022-12-09 13:05:15 +0100

    prints a message depending of the number of arguments passed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args = 0) {
+  console.log('No argument');
+}
+if (args = 1) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
