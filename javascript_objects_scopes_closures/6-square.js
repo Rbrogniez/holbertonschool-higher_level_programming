a1a5e07b44ab6d39a2fe56c94236283f4bdb6e5e
Rbrogniez <5013@holbertonschool.com>
2022-12-13 15:47:07 +0100

    Create an instance method called charPrint(c) that prints the rectangle using the character c

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,26 @@
 
 const Rectangle = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
+module.exports = class Square extends Rectangle
+{
+  constructor (size)
+  {
     super(size, size);
   }
-};
 
-charPrint(c);
-for (let i = 0; i < this.height; i++) {
-  for (let j = 0; j < this.width; j++) {
-    process.stdout.write(c);
-  }
-  process.stdout.write('\n');
+	charPrint (c)
+	{
+		if (c == null)
+		{
+			c = 'X';
+		}
+		for (let i = 0; i < this.height; i++)
+		{
+			for(let j = 0; j < this.width; j++)
+			{
+				process.stdout.write(c);
+			}
+		process.stdout.write('\n');
+		}
+	}
 }
