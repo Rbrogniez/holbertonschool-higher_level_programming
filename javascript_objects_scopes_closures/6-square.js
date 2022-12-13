2b2b2e340ab4d6c41b271f08d6022ce9e539796c
Rbrogniez <5013@holbertonschool.com>
2022-12-13 15:48:12 +0100

    semistandard fix

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,26 +3,20 @@
 
 const Rectangle = require('./5-square');
 
-module.exports = class Square extends Rectangle
-{
-  constructor (size)
-  {
+module.exports = class Square extends Rectangle {
+  constructor (size) {
     super(size, size);
   }
 
-	charPrint (c)
-	{
-		if (c == null)
-		{
-			c = 'X';
-		}
-		for (let i = 0; i < this.height; i++)
-		{
-			for(let j = 0; j < this.width; j++)
-			{
-				process.stdout.write(c);
-			}
-		process.stdout.write('\n');
-		}
-	}
-}
+  charPrint (c) {
+    if (c == null) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      process.stdout.write('\n');
+    }
+  }
+};
