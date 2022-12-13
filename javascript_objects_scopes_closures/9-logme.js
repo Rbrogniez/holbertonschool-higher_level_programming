6919076a920fbf174690d4c369db37c73bde994f
Rbrogniez <5013@holbertonschool.com>
2022-12-13 16:56:47 +0100

    put counter outside of function

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-exports.logMe = function (item) {
-	// Create a counter variable to keep track of the number of arguments printed
-	let counter = 0;
+// Prints the number of arguments already printed and the new
+// argument value
 
-	// Return a function that will be called each time the logMe function is called
-	return function() {
-	  // Increment the counter variable
-	  counter++;
-	  // Print the current count and the current argument value
-	  console.log(`${counter}: ${item}`);
-	}
-  }
+let counter = 0;
+exports.logMe = function (item)
+	{
+
+	console.log(`${counter}: ${item}`);
+	counter++;
+  };
