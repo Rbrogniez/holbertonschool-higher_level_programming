176ca29986230d2317153003ba0fdabb62e9f2f5
Rbrogniez <5013@holbertonschool.com>
2022-12-13 15:22:23 +0100

    fix semistand

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -9,12 +9,10 @@ module.exports = class Square extends Rectangle {
   }
 };
 
-charPrint(c) {
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c);
-      }
-      process.stdout.write('\n');
-    }
+charPrint(c);
+for (let i = 0; i < this.height; i++) {
+  for (let j = 0; j < this.width; j++) {
+    process.stdout.write(c);
   }
-
+  process.stdout.write('\n');
+}
